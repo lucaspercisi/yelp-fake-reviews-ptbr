b66dd3af4f8cca11711c68462956e84a22dab4f0
Lucas Percisi <lucas_percisi@hotmail.com>
2023-11-30 15:59:43 -0300

    Resultado do conjunto de dados em inglês balanceado

--- scripts/5_final_hybrid_model.py
+++ scripts/5_final_hybrid_model.py
@@ -71,8 +71,8 @@ def clean_text_en(text):
     return text
 
     
-# url_dataset = 'https://raw.githubusercontent.com/lucaspercisi/yelp-fake-reviews-ptbr/main/Datasets/portuguese/yelp-fake-reviews-dataset-pt-pos-tagged.csv'
-url_dataset = 'https://raw.githubusercontent.com/lucaspercisi/yelp-fake-reviews-ptbr/main/Datasets/english/yelp-fake-reviews-dataset-en.csv'
+url_dataset = 'https://raw.githubusercontent.com/lucaspercisi/yelp-fake-reviews-ptbr/main/Datasets/portuguese/yelp-fake-reviews-dataset-pt-pos-tagged.csv'
+# url_dataset = 'https://raw.githubusercontent.com/lucaspercisi/yelp-fake-reviews-ptbr/main/Datasets/english/yelp-fake-reviews-dataset-en.csv'
 yelp_df = pd.read_csv(url_dataset)
 
 # #Contando pontuação
@@ -85,7 +85,7 @@ yelp_df['capital_count'] = yelp_df['content'].apply(lambda x: len([c for c in st
 yelp_df['word_count'] = yelp_df['content'].apply(lambda x: len(str(x).split(" ")))
 
 #limpando conteudo textual
-yelp_df['cleaned_content'] = yelp_df['content'].apply(clean_text_en)
+yelp_df['cleaned_content'] = yelp_df['content'].apply(clean_text)
 
 #limpando conteudo textual com tag gramtical e convertendo para string
 # yelp_df['cleaned_content_tagged'] = yelp_df['content_tagged'].apply(extract_words_from_tagged_content)
@@ -106,7 +106,8 @@ else:
     df_verdadeiros = df_verdadeiros.sample(num_falsos, random_state=42)
 
 yelp_df_balanceado = pd.concat([df_falsos, df_verdadeiros])
-yelp_df_sample = yelp_df_balanceado.copy()
+# yelp_df_sample = yelp_df_balanceado.copy()
+yelp_df_sample = yelp_df.copy()
 
 
 X = yelp_df_sample['cleaned_content']
@@ -289,7 +290,7 @@ def run_and_save_results(clf, X, y, classifier_name, vectorizer, results_df, fea
     # Preparando os resultados
     features_used = ', '.join(features_useds.columns)
     results = {
-        'scenario' : 'en_equal',
+        'scenario' : 'pt_non_equal',
         'classifier': classifier_name,
         'vectorizer': vectorizer,
         'features_used': features_used,
@@ -319,7 +320,7 @@ def run_and_save_results(clf, X, y, classifier_name, vectorizer, results_df, fea
     updated_results_df = pd.concat([results_df, pd.DataFrame([results])], axis=0, ignore_index=True)
 
     agora = datetime.now().strftime("%Y%m%d_%H%M%S")
-    nome_arquivo = f"{caminho_base}/resultados_en_equal_{agora}_final_certo.csv"
+    nome_arquivo = f"{caminho_base}/resultados_pt_non_equal_{agora}_final_certo.csv"
     
     updated_results_df.to_csv(nome_arquivo, index=False)
     
